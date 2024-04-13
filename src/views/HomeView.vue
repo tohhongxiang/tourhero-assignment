9138f3dc9b53742d5414d42815bf8e263e80a88a
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-04-13 23:22:29 +0800

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <main>
+    <p class="text-7xl font-bold">Home View</p>
+    <Button>Click me</Button>
+  </main>
+</template>
